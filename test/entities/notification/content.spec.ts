14c980f07582ad30db929441ec603b661423041f
Leonardo Delgado <leonardodel52@gmail.com>
2022-12-13 23:41:19 -0300

    :sparkles: feat: Implementando persistência da notificação

---
+++ test/entities/notification/content.spec.ts
@@ -0,0 +1,17 @@
+import { Content } from '../../../src/application/entities/notification/content';
+
+describe('Content', () => {
+  it('should be able to create a notification content', () => {
+    const content = new Content('Você acabou de receber uma ligação no Teams');
+
+    expect(content).toBeTruthy();
+  });
+
+  it('should not be able to create a notification content with less than 5 characters', () => {
+    expect(() => new Content('Test')).toThrowError();
+  });
+
+  it('should not be able to create a notification content with more than 240 characters', () => {
+    expect(() => new Content('Test'.repeat(61))).toThrowError();
+  });
+});
---
+++ test/use-cases/send-notificatin.spec.ts
@@ -0,0 +1,20 @@
+import { InMemoryNotificationsRepository } from '../../test/repositories/in-memory-notifications-repository';
+import { SendNotification } from '../../src/application/use-cases/send-notification';
+
+describe('SendNotification', () => {
+  it('should be able to send a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const sendNotification = new SendNotification(notificationsRepository);
+
+    const { notification } = await sendNotification.execute({
+      content: 'Nova solicitação de amizade',
+      category: 'social',
+      recipientId: 'example-recipient-id',
+    });
+
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toStrictEqual(
+      notification,
+    );
+  });
+});
